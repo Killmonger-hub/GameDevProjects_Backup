70eeb8fd4c57f55439beed82d43d5bf15d0e40e0
Killmonger-hub <aaronthomas25700@gmail.om>
2021-02-26 16:29:51 +0530

    Dated 26/2/2021

--- Unity 2017/LandMass_Generation/Assets/Scripts/TextureGenerator.cs
+++ Unity 2017/LandMass_Generation/Assets/Scripts/TextureGenerator.cs
@@ -1,9 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public static class TextureGenerator {
-
-	public static Texture2D TextureFromColourMap(Color[] colourMap, int width, int height) {
+public static class TextureGenerator 
+{
+	public static Texture2D TextureFromColourMap(Color[] colourMap, int width, int height) 
+	{
 		Texture2D texture = new Texture2D (width, height);
 		texture.filterMode = FilterMode.Point;
 		texture.wrapMode = TextureWrapMode.Clamp;
@@ -11,20 +12,21 @@ public static class TextureGenerator {
 		texture.Apply ();
 		return texture;
 	}
-
-
-	public static Texture2D TextureFromHeightMap(float[,] heightMap) {
-		int width = heightMap.GetLength (0);
-		int height = heightMap.GetLength (1);
+		
+	public static Texture2D TextureFromHeightMap(HeightMap heightMap) 
+	{
+		int width = heightMap.values.GetLength (0);
+		int height = heightMap.values.GetLength (1);
 
 		Color[] colourMap = new Color[width * height];
-		for (int y = 0; y < height; y++) {
-			for (int x = 0; x < width; x++) {
-				colourMap [y * width + x] = Color.Lerp (Color.black, Color.white, heightMap [x, y]);
+		for (int y = 0; y < height; y++) 
+		{
+			for (int x = 0; x < width; x++) 
+			{
+				colourMap [y * width + x] = Color.Lerp (Color.black, Color.white, Mathf.InverseLerp(heightMap.minValue, heightMap.maxValue, heightMap.values [x, y]));
 			}
 		}
 
 		return TextureFromColourMap (colourMap, width, height);
 	}
-
 }
