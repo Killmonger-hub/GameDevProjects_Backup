cdc1e78beee9b90acc09bed804f603a8905bf273
Killmonger-hub <78304240+Killmonger-hub@users.noreply.github.com>
2021-03-06 15:57:21 +0530

    Dated 3/6/2021

---
+++ Unity 2017/2D_Platformer/Assets/Scripts/GameMaster.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMaster : MonoBehaviour
+{
+    public Transform playerPrefab;
+    public Transform spawnPoint;
+    public int spawnDelay = 2;
+    public static GameMaster gm;
+
+    void Start()
+    {
+        if(gm == null)
+        {
+            gm = GameObject.FindGameObjectWithTag("GM").GetComponent<GameMaster>();
+        }
+    }
+
+    public IEnumerator RespawnPlayer()
+    {
+        Debug.Log("TODO: Play Spawn Sound");
+        yield return new WaitForSeconds(spawnDelay);
+
+        Instantiate(playerPrefab, spawnPoint.position, spawnPoint.rotation);
+        Debug.Log("TODO: Add Spawn Particles");
+    }
+
+    public static void KillPlayer(Player player)
+    {
+        Destroy(player.gameObject);
+        gm.StartCoroutine(gm.RespawnPlayer());
+    }
+}
---
+++ Unity 2017/2D_Platformer/Assets/Scripts/Player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [System.Serializable]
+    public class PlayerStats
+    {
+        public int health = 100;
+    }
+
+    public PlayerStats playerStats = new PlayerStats();
+
+    public int fallBoundary = -20;
+
+    void Update()
+    {
+        if(transform.position.y <= fallBoundary)
+        {
+            DamagePlayer(999);
+        }
+    }
+
+    public void DamagePlayer(int damage)
+    {
+        playerStats.health -= damage;
+        if(playerStats.health <= 0)
+        {
+            Debug.Log("You Ded!!!");
+            GameMaster.KillPlayer(this);
+        }
+    }
+}
